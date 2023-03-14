c1ff9a9afc0632b8cb0fec63d9c7c01b4295caaf
Roman Boiko <roma685235423@icloud.com>
2023-03-14 23:43:48 +0300

    Messengers info added

--- ContactsList/ViewControllers/ContactScreen/ContactCell.swift
+++ ContactsList/ViewControllers/ContactScreen/ContactCell.swift
@@ -92,3 +92,73 @@ final class ContactCell: UITableViewCell {
         return UIImage(data: data)!
     }
 }
+
+/*
+ private func configureIcons(messengers: messengersIconNames) {
+     let iconSize: CGFloat = 30
+     var iconConstraints = [NSLayoutConstraint]()
+     var icons = [UIImageView]()
+
+     if let telegramIcon = messengers.telegram {
+         let imageView = createIconImageView(iconName: telegramIcon, iconSize: iconSize)
+         icons.append(imageView)
+     }
+
+     if let whatsAppIcon = messengers.whatsApp {
+         let imageView = createIconImageView(iconName: whatsAppIcon, iconSize: iconSize)
+         icons.append(imageView)
+     }
+
+     if let viberIcon = messengers.viber {
+         let imageView = createIconImageView(iconName: viberIcon, iconSize: iconSize)
+         icons.append(imageView)
+     }
+
+     if let signalIcon = messengers.signal {
+         let imageView = createIconImageView(iconName: signalIcon, iconSize: iconSize)
+         icons.append(imageView)
+     }
+
+     if let threemaIcon = messengers.threema {
+         let imageView = createIconImageView(iconName: threemaIcon, iconSize: iconSize)
+         icons.append(imageView)
+     }
+
+     if let phoneIcon = messengers.phone {
+         let imageView = createIconImageView(iconName: phoneIcon, iconSize: iconSize)
+         icons.append(imageView)
+     }
+
+     if let emailIcon = messengers.email {
+         let imageView = createIconImageView(iconName: emailIcon, iconSize: iconSize)
+         icons.append(imageView)
+     }
+
+     for (index, iconView) in icons.enumerated() {
+         contactCellView.addSubview(iconView)
+
+         if index == 0 {
+             iconConstraints.append(iconView.leftAnchor.constraint(equalTo: contactImage.rightAnchor, constant: 12))
+         } else {
+             let previousIcon = icons[index-1]
+             iconConstraints.append(iconView.leftAnchor.constraint(equalTo: previousIcon.rightAnchor, constant: 8))
+         }
+
+         iconConstraints.append(iconView.centerYAnchor.constraint(equalTo: contactCellView.centerYAnchor))
+         iconConstraints.append(iconView.heightAnchor.constraint(equalToConstant: iconSize))
+         iconConstraints.append(iconView.widthAnchor.constraint(equalToConstant: iconSize))
+     }
+
+     NSLayoutConstraint.activate(iconConstraints)
+ }
+
+ private func createIconImageView(iconName: String, iconSize: CGFloat) -> UIImageView {
+     let imageView = UIImageView()
+     imageView.translatesAutoresizingMaskIntoConstraints = false
+     imageView.image = UIImage(named: iconName)
+     imageView.contentMode = .scaleAspectFit
+     imageView.layer.cornerRadius = iconSize / 2
+     imageView.clipsToBounds = true
+     return imageView
+ }
+ */
